978f3c8b9834ec84025c929b9b7f89911dfc7b93
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-12-18 12:55:16 -0100

    commentsList works

--- blog/client/src/components/CommentList.js
+++ blog/client/src/components/CommentList.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const CommentList = () => {
-  return (
-    <div>
-      <h1>List Comment</h1>
-    </div>
-  );
+const CommentList = ({ postId }) => {
+  const [comments, setComments] = useState([]);
+
+  const fetchComments = async () => {
+    const res = await axios.get(
+      `http://localhost:4000/posts/${postId}/comments`
+    );
+    setComments(res.data);
+  };
+
+  useEffect(() => {
+    fetchComments();
+  }, []);
+
+  const renderedComments = comments.map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
 };
 
 export default CommentList;
--- blog/client/src/components/PostList.js
+++ blog/client/src/components/PostList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
 
 const PostList = () => {
   const [posts, setPosts] = useState({});
@@ -23,11 +24,13 @@ const PostList = () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
+          <CommentList postId={post.id} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
     );
   });
+
   return (
     <div className="d-flex flex-row flex-wrap justify-content-between">
       {renderedPosts}
