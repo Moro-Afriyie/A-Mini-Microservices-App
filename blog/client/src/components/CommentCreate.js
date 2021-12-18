b25699815573412e9963063050a7d8fb77a5c1da
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-12-18 12:27:41 -0100

    create CommentList and CommentCreate components

---
+++ blog/client/src/components/CommentCreate.js
@@ -0,0 +1,12 @@
+import React from "react";
+import axios from "axios";
+
+const CommentCreate = () => {
+  return (
+    <div>
+      <h1>Create Comment</h1>
+    </div>
+  );
+};
+
+export default CommentCreate;
---
+++ blog/client/src/components/CommentList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import axios from "axios";
+
+const CommentList = () => {
+  return (
+    <div>
+      <h1>List Comment</h1>
+    </div>
+  );
+};
+
+export default CommentList;
