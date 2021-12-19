a68e750ac59ed9c88658d889fe3f257863b1e674
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-12-19 13:03:32 -0100

    fix the port bug

--- blog/client/src/components/PostList.js
+++ blog/client/src/components/PostList.js
@@ -7,7 +7,7 @@ const PostList = () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const res = await axios.get("http://localhost:6000/posts");
+    const res = await axios.get("http://localhost:4005/posts");
     setPosts(res.data);
   };
 
