540f2c33187370c9b7c75ddd31ef90d287ede450
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-12-19 19:43:39 -0100

    done with the moderation service

---
+++ blog/moderation/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// listens to events for comment created event
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+  if (type === "CommentCreated") {
+    const status = data.content.includes("orange") ? "rejected" : "approved";
+
+    axios
+      .post("http://localhost:7000/events", {
+        type: "CommentModerated",
+        data: {
+          id: data.id,
+          postId: data.postId,
+          status,
+          content: data.content,
+        },
+      })
+      .catch((err) => console.log(err));
+  }
+});
+
+app.listen(4006, () => console.log("server listening on port 4006"));
