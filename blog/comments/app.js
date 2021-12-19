19ae80bb8e9e8a729d6e1c16090191188523b867
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-12-19 19:52:28 -0100

    add a logic to emit events to the event bus

--- blog/comments/app.js
+++ blog/comments/app.js
@@ -21,7 +21,7 @@ app.post("/posts/:id/comments", async (req, res) => {
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: id, content });
+  comments.push({ id: id, content, staus: "pending" });
 
   commentsByPostId[req.params.id] = comments;
 
@@ -32,14 +32,40 @@ app.post("/posts/:id/comments", async (req, res) => {
       id,
       content,
       postId: req.params.id,
+      staus: "pending",
     },
   });
 
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
+// route to listen to events
+app.post("/events", async (req, res) => {
   console.log("Received Event: ", req.body.type);
+
+  const { type, data } = req.body;
+  if (type === "CommentModerated") {
+    // get the comment by PostId and update the status
+    const { id, postId, status, content } = data;
+
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+
+    await axios.post("http:localhost:7000/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        postId,
+        content,
+        status,
+      },
+    });
+  }
 
   res.send({});
 });
