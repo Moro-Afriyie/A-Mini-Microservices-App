2c2754df16721cd9fbe5db3c3eabff024921e8e0
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-12-19 11:41:39 -0100

    implement a logic to emit an event to the event bus

--- blog/posts/app.js
+++ blog/posts/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { randomBytes } = require("crypto");
+const { default: axios } = require("axios");
 
 const app = express();
 
@@ -14,7 +15,7 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
 
@@ -22,6 +23,15 @@ app.post("/posts", (req, res) => {
     id,
     title,
   };
+
+  // emit the event to the event bus
+  await axios.post("http://localhost:7000/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
 
   res.status(201).send(posts[id]);
 });
