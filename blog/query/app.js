958fae9e3d8bb60d8a609df8f808a5f611de1d0e
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-12-19 12:06:18 -0100

    add a route to listen to events

--- blog/query/app.js
+++ blog/query/app.js
@@ -11,6 +11,9 @@ app.use(express.urlencoded({ extended: true }));
 // route to send back the posts and comments of a particular post
 app.get("/posts", (req, res) => {});
 
+// route to listen to the event emitted by the event bus
+app.post("/events", (req, res) => {});
+
 app.listen(6000, () => {
   console.log("sever listening on port 6000");
 });
