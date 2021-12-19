7e4d6de84c1c637cde1a2a39985576c72ce10e49
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-12-19 20:34:01 -0100

    add a logic to store the events

--- blog/event-bus/app.js
+++ blog/event-bus/app.js
@@ -6,8 +6,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
+
+  // store the events
+  events.push(event);
 
   // posts service
   axios
@@ -30,6 +35,10 @@ app.post("/events", (req, res) => {
     .catch((err) => console.log(err));
 
   res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(7000, () => {
