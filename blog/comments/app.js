f20a7129fb99497728ca73e6562e70775e110258
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-12-19 11:47:30 -0100

    add the logic to emit an event to the event bus

--- blog/comments/app.js
+++ blog/comments/app.js
@@ -14,7 +14,7 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { content } = req.body;
 
@@ -23,6 +23,16 @@ app.post("/posts/:id/comments", (req, res) => {
   comments.push({ id: id, content });
 
   commentsByPostId[req.params.id] = comments;
+
+  // emit the event to the event bus
+  await axios.post("http://localhost:7000/events", {
+    type: "CommentCreated",
+    data: {
+      id,
+      content,
+      postId: req.params.id,
+    },
+  });
 
   res.status(201).send(comments);
 });
